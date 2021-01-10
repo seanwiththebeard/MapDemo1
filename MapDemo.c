291ead38d83f7d77b510e8c88c6594e36756a1b9
F8Sean <42120363+F8Sean@users.noreply.github.com>
2021-01-09 22:23:45 -0500

    updated from 8bitworkshop.com

--- MapDemo.c
+++ MapDemo.c
@@ -3,6 +3,7 @@
 #include <c64.h>
 #include <cbm_petscii_charmap.h>
 #include <peekpoke.h>
+#include <joystick.h>
 
 #include "c64_bitmapmode.h"
 //#link "c64_bitmapmode.c"
@@ -15,13 +16,27 @@ byte mapData[200][200];
 //Viewport
 byte viewportPosX = 2;
 byte viewportPosY = 2;
-byte viewportWidth = 22;
-byte viewportHeight = 22;
+byte viewportWidth = 16;
+byte viewportHeight = 16;
 int viewportMemYOffset;
 int ViewportMemPos;
 //Camera Position
 int offsetX, offsetY = 0;
 int x, y, a, b = 0;
+
+void rasterWait(unsigned char line) {
+  while (VIC.rasterline < line) ;
+}
+
+void CheckInput()
+{
+  char joy;
+  joy = joy_read(0);
+  if (JOY_UP(joy)) offsetY-=2;
+  if (JOY_LEFT(joy)) offsetX-=2;
+  if (JOY_RIGHT(joy)) offsetX+=2;
+  if (JOY_DOWN(joy)) offsetY+=2; 
+}
 
 void DrawScreen()
 {
@@ -41,21 +56,30 @@ void DrawScreen()
   a = offsetX;
   b = offsetY;
   
-  for(y = 0; y != viewportHeight; y++)
+  //y = viewportHeight;
+  for(y = 0; y < viewportHeight; y++)
+  //while(y--)
   {
+    //Wrap the map data y reference
     if (b == mapHeight)
         b = 0;
     if (b < 0)
-      b +=mapHeight;    
-    for(x = 0; x != viewportWidth; x++)
+      b +=mapHeight;
+    
+    //x = viewportWidth;
+    for(x = 0; x < viewportWidth; x++)
+    //while(x--)
     {
+      //Wrap the map data X reference
       if (a == mapWidth)
           a = 0;
       if (a < 0)
           a += mapWidth;
+      
       //cputcxy(x + viewportPosX, y + viewportPosY, mapData[a][b]);
       //POKE(viewportAddress + x + 40*(y + viewportPosY), mapData[a][b]);
       POKE(ViewportMemPos + x + viewportPosX, mapData[a][b]);
+      
       a++;
     }
     ViewportMemPos += 40;
@@ -71,6 +95,7 @@ void main(void)
   byte signpost = 'X';
   
   viewportMemYOffset = viewportPosY * 40;
+  joy_install (joy_static_stddrv);
   clrscr();  
   
   setcolortextmode();
@@ -102,8 +127,8 @@ void main(void)
   
   while(1)
   {
+    //rasterWait(255);
+    CheckInput();
     DrawScreen();
-    offsetX += 2;
-    offsetY += 2;    
   }
 }
