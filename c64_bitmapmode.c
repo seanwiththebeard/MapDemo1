d1ffd94be3c6dd51a10519c50d830a48b1d9d7ed
F8Sean <42120363+F8Sean@users.noreply.github.com>
2021-01-31 16:03:01 -0500

    Custom character definition

--- c64_bitmapmode.c
+++ c64_bitmapmode.c
@@ -19,8 +19,34 @@ void settextmode()
 void setcolortextmode()
 {
   int i = 0;
-  //VIC.ctrl1 = 0x1b;
-  //VIC.ctrl2 = 0x18;
+  
+  //Copy character set
+  POKE(0x0034, 48);  // Reserve RAM
+  POKE(0x0038, 48); // Reserve RAM
+  POKE(0xDC0E, PEEK(0xDC0E)&254); // Pause Keyscan
+  POKE(0x0001, (PEEK(0x0001)&251)); // Character ROM select
+  
+  //Copy the character set to the new RAM location
+  for(i = 0; i < 1024; i++)
+  {
+    POKE(i + 0x3000, PEEK(i + 0xD000));
+  }
+  
+  POKE(0x0001, (PEEK(0x0001)|4)); // Character ROM de-select, back to IO
+  POKE(0xDC0E, PEEK(0xDC0E)|1); // Resume Keyscan
+  
+  //Redirect the character set address, select uppercase set
+  POKE (0xD018, (PEEK(0xD018)&240) +12);
+  
+  for(i = 0; i < 8; i++)
+  {
+    POKE(i + 0x3000, 255);
+  }
+  
+  //Select first character set
+  //POKE(0xD018, 21);
+  //Select second character set
+  //POKE(0xD018, 23);
   
   //Background / 00
   POKE(0xD021, 0);
@@ -32,4 +58,6 @@ void setcolortextmode()
   //Foreground / 11 / 0-7 single, 8-15 multi
   for (i = 0; i < 1000; i++)
     POKE(0xD800 + i, 1);
+  
+  
 }
