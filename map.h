e87e258c3ca47033c70fe73b2563b27a7e97e999
F8Sean <42120363+F8Sean@users.noreply.github.com>
2021-12-18 23:20:08 -0500

    Delete map.h

--- map.h
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-void InitializeMapData();
-void DrawTile(byte index, byte xpos, byte ypos);
-void DrawMap();
-void DrawCharacters();
-void CheckInput();
-void DrawCharacter(byte index, byte xpos, byte ypos);
-void MapUpdate();
