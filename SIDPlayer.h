303890409f95f4dae547c37239e4acffea4b0098
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2022-08-18 13:30:55 -0400

    Delete SIDPlayer.h

--- SIDPlayer.h
+++
@@ -1,9 +0,0 @@
-extern char SIDFILE[];
-extern void SIDINIT(); 
-extern void SIDPLAY();
-extern void STOPSID();
-extern void PlaySID(void);
-extern void StopSID(void);
-
-#define SIDSIZE (1024)*2
-#define SIDLOAD 0xB000
